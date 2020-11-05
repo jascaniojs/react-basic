612025dfc3f7ffe4660b11d5a1a09f85663b9ed7
Juan Ascanio <jascaniojs@gmail.com>
2020-11-05 17:06:26 -0500

    Login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { loginRequest } from '../actions';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 import '../assets/styles/components/Login.scss';
-import { Link } from 'react-router-dom';
 
-const Login = () => {
+const Login = (props) => {
+  const [form, setValues] = useState({ email: '' });
+
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(form);
+    const { loginRequest, history } = props;
+    loginRequest(form).then(history.push('/'));
+  };
+
   return (
     <section className='login'>
       <section className='login__container'>
         <h2>Inicia sesión</h2>
-        <form className='login__container--form'>
-          <input className='input' type='text' placeholder='Correo' />
-          <input className='input' type='password' placeholder='Contraseña' />
-          <button type='button' className='button'>
+        <form onSubmit={handleSubmit} className='login__container--form'>
+          <input
+            name='email'
+            onChange={handleInput}
+            className='input'
+            type='text'
+            placeholder='Correo'
+          />
+          <input
+            onChange={handleInput}
+            name='password'
+            className='input'
+            type='password'
+            placeholder='Contraseña'
+          />
+          <button type='submit' className='button'>
             Iniciar sesión
           </button>
           <div className='login__container--remember-me'>
@@ -42,4 +72,8 @@ const Login = () => {
   );
 };
 
-export default Login;
+const mapDispatchToProps = {
+  loginRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 import App from './routes/App';
 
@@ -171,7 +171,33 @@ const initialState = {
   ],
 };
 
-const store = createStore(reducer, initialState);
+const logger = (store) => (next) => (action) => {
+  console.log('dispatching', action);
+  const result = next(action);
+  console.log('next state', store.getState());
+  return result;
+};
+
+const crashReporter = (store) => (next) => (action) => {
+  try {
+    return next(action);
+  } catch (err) {
+    console.error('Caught an exception!', err);
+    Raven.captureException(err, {
+      extra: {
+        action,
+        state: store.getState(),
+      },
+    });
+    throw err;
+  }
+};
+
+const store = createStore(
+  reducer,
+  initialState,
+  applyMiddleware(logger, crashReporter)
+);
 
 ReactDOM.render(
   <Provider store={store}>
